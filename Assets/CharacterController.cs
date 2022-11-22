ef1d2e4024a61acb73992904a7d1be8b381dbd59
anyar06 <113103456+anyar06@users.noreply.github.com>
2022-11-22 12:09:20 +0000

    Added Texture - Updated Code
    Finer details - up to jumping code

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -4,24 +4,53 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    public float maxSpeed = 0.9f;
+    public float maxSpeed;
+    public float normalSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
+
     float rotation = 0.0f;
     float camRotation = 0.0f;
     GameObject cam;
     Rigidbody myRigidbody;
 
-    float rotationSpeed = 2.0f;
-    float camRotationSpeed = 1.5f;
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
+
+    public float rotationSpeed = 2.0f;
+    public float camRotationSpeed = 1.5f;
+
+    public float maxSprint = 5.0f;
+    float sprintTimer;
 
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        sprintTimer = maxSprint;
+
         cam = GameObject.Find("Main Camera");
         myRigidbody = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        //transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
-        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed = sprintSpeed;
+        } else
+        {
+            maxSpeed = normalSpeed;
+        }
+        
+        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
         myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
